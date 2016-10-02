44317f0c5fa2e7fd08ef0f8500d33ac55ef1c168
Michael Lin <Terragnome@gmail.com>
2016-10-02 10:18:52 -0700

    Updated .gitignore

--- Assets/Scripts/Models/Player.cs
+++ Assets/Scripts/Models/Player.cs
@@ -2,14 +2,56 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+	float walkSpeed = 2f;
 
-	// Use this for initialization
+	float dashSpeed = 7f;
+	float dashTime = 0.3f;
+	bool isDashing = false;
+	float dashTimeLeft = 0f;
+
+  // Use this for initialization
 	void Start () {
-	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		float dT = Time.deltaTime;
+		float curWalkSpeed = walkSpeed;
+
+		bool isWalkForward = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+		bool isWalkBack = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+		bool isWalkLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+		bool isWalkRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+
+		if( !isDashing ){
+			bool hasDashed = Input.GetKeyDown(KeyCode.Space);
+			if(hasDashed){
+				isDashing = true;
+				dashTimeLeft = dashTime;
+			}
+		}else{
+			curWalkSpeed += dashSpeed;
+			if(dashTimeLeft > 0){
+				dashTimeLeft -= dT;
+				if(dashTimeLeft <= 0){
+					isDashing = false;
+					dashTimeLeft = 0f;
+				}
+			}
+		}
+
+		if(isWalkBack && isWalkForward){
+		}else if(isWalkBack){
+			this.gameObject.transform.Translate(Vector3.back*dT*curWalkSpeed);
+		}else if(isWalkForward){
+			this.gameObject.transform.Translate(Vector3.forward*dT*curWalkSpeed);
+		}
+
+		if(isWalkLeft && isWalkRight){
+		}else if(isWalkLeft){
+			this.gameObject.transform.Translate(Vector3.left*dT*curWalkSpeed);
+		}else if(isWalkRight){
+			this.gameObject.transform.Translate(Vector3.right*dT*curWalkSpeed);
+		}
 	}
 }

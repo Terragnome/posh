bc5c99eb6c0ca61020aa117e90b6fd83d3a55587
Michael Lin <Terragnome@gmail.com>
2016-10-09 15:16:07 -0700

    Update portable script to add rigidbody and collider

--- Assets/Scripts/Models/Portable.cs
+++ Assets/Scripts/Models/Portable.cs
@@ -6,14 +6,38 @@ public class Portable : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Rigidbody rigidBody = rb;
+		Collider sphereCollider = collider;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	}
 
+	Collider collider {
+		get {
+			SphereCollider sphereCollider = GetComponent<SphereCollider>();
+			if(sphereCollider == null){
+				sphereCollider = gameObject.AddComponent<SphereCollider>();
+				sphereCollider.radius = 0.5f;
+			}
+			return sphereCollider;
+		}
+	}
+
 	Rigidbody rb {
-		get { return GetComponent<Rigidbody>(); }
+		get {
+			Rigidbody rigidbody = GetComponent<Rigidbody>();
+			if(rigidbody == null){
+				rigidbody = gameObject.AddComponent<Rigidbody>();
+				rigidbody.angularDrag = 5;
+				rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+				rigidbody.drag = 5;
+				rigidbody.mass = 1;
+				rigidbody.useGravity = true;
+			}
+			return rigidbody;
+		}
 	}
 
 	public void Lift() {
--- Assets/Scripts/Models/Usable.cs
+++ Assets/Scripts/Models/Usable.cs
@@ -5,12 +5,21 @@ public class Usable : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	Collider collider {
+		get {
+			BoxCollider boxCollider = GetComponent<BoxCollider>();
+			if(boxCollider == null){
+				boxCollider = gameObject.AddComponent<BoxCollider>();
+				boxCollider.size = new Vector3(1f, 1f, 1f);
+			}
+			return boxCollider;
+		}
 	}
 
 	public void Use(float dT) {
